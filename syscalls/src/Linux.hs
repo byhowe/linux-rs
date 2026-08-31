c00182d69a45d4b6d89d008d6541d8859f4a464e
Ömer Faruk Çavuş <37745048+byhowe@users.noreply.github.com>
2026-08-31 21:07:37 +0300

    hs: Add bitsets required for `mmap`

--- syscalls/src/Linux.hs
+++ syscalls/src/Linux.hs
@@ -8,13 +8,13 @@ import Data.Set (Set)
 import Data.Set qualified as Set
 import Data.Word (Word64)
 
-data Arch = X86_64 | Aarch64 | Riscv64
+data Arch = X86_64 | AArch64 | RISCV64
   deriving (Eq, Ord, Enum, Bounded)
 
 instance Show Arch where
   show X86_64 = "x86_64"
-  show Aarch64 = "aarch64"
-  show Riscv64 = "riscv64"
+  show AArch64 = "aarch64"
+  show RISCV64 = "riscv64"
 
 allArchs :: Set Arch
 allArchs = Set.fromList [minBound .. maxBound]
@@ -37,26 +37,26 @@ data PrimType
 data Mutability
   = Const
   | Mut
-  deriving (Eq, Show)
+  deriving (Show, Eq)
 
 data ArrayLen
   = NullTerminated
   | ParamLen String
   | FixedLen Int
-  deriving (Eq, Show)
+  deriving (Show, Eq)
 
 data EnumValue = EnumValue
   { name :: String
   , value :: Word64
   , archs :: Set Arch
   }
-  deriving (Eq, Show)
+  deriving (Show, Eq)
 
 data EnumDef = EnumDef
   { name :: String
   , values :: [EnumValue]
   }
-  deriving (Eq, Show)
+  deriving (Show, Eq)
 
 data BitsetField
   = FieldFlag
@@ -69,14 +69,14 @@ data BitsetField
       , width :: Int
       , enumDef :: EnumDef
       }
-  deriving (Eq, Show)
+  deriving (Show, Eq)
 
 data Bitset = Bitset
   { name :: String
   , backing :: PrimType
   , fields :: [BitsetField]
   }
-  deriving (Eq, Show)
+  deriving (Show, Eq)
 
 data Type
   = TypeVoid
@@ -101,4 +101,73 @@ data Type
       { name :: String
       , target :: Type
       }
-  deriving (Eq, Show)
+  deriving (Show, Eq)
+
+protBitset :: Bitset
+protBitset =
+  Bitset
+    "prot"
+    U32
+    [ FieldFlag "read" 0 allArchs
+    , FieldFlag "write" 1 allArchs
+    , FieldFlag "exec" 2 allArchs
+    , FieldFlag "sem" 3 allArchs
+    , FieldFlag "bti" 4 (Set.singleton AArch64)
+    , FieldFlag "mte" 5 (Set.singleton AArch64)
+    , FieldFlag "growsdown" 24 allArchs
+    , FieldFlag "growsup" 25 allArchs
+    ]
+
+hugePageSizeEnum :: EnumDef
+hugePageSizeEnum =
+  EnumDef
+    "huge_page_size"
+    [ EnumValue "16kb" 14 allArchs
+    , EnumValue "64kb" 16 allArchs
+    , EnumValue "512kb" 19 allArchs
+    , EnumValue "1mb" 20 allArchs
+    , EnumValue "2mb" 21 allArchs
+    , EnumValue "8mb" 23 allArchs
+    , EnumValue "16mb" 24 allArchs
+    , EnumValue "32mb" 25 allArchs
+    , EnumValue "256mb" 28 allArchs
+    , EnumValue "512mb" 29 allArchs
+    , EnumValue "1gb" 30 allArchs
+    , EnumValue "2gb" 31 allArchs
+    , EnumValue "16gb" 34 allArchs
+    ]
+
+mapBitset :: Bitset
+mapBitset =
+  Bitset
+    "map"
+    U32
+    [ FieldEnum
+        0
+        4
+        ( EnumDef
+            "map_type"
+            [ EnumValue "shared" 1 allArchs
+            , EnumValue "private" 2 allArchs
+            , EnumValue "shared_validate" 3 allArchs
+            , EnumValue "droppable" 8 allArchs
+            ]
+        )
+    , FieldFlag "fixed" 4 allArchs
+    , FieldFlag "anonymous" 5 allArchs
+    , FieldFlag "32bit" 6 (Set.singleton X86_64)
+    , FieldFlag "above4g" 7 (Set.singleton X86_64)
+    , FieldFlag "growsdown" 8 allArchs
+    , FieldFlag "denywrite" 11 allArchs
+    , FieldFlag "executable" 12 allArchs
+    , FieldFlag "locked" 13 allArchs
+    , FieldFlag "noreserve" 14 allArchs
+    , FieldFlag "populate" 15 allArchs
+    , FieldFlag "nonblock" 16 allArchs
+    , FieldFlag "stack" 17 allArchs
+    , FieldFlag "hugetlb" 18 allArchs
+    , FieldFlag "sync" 19 allArchs
+    , FieldFlag "fixed_noreplace" 20 allArchs
+    , FieldEnum 26 6 hugePageSizeEnum
+    , FieldFlag "uninitialized" 26 allArchs
+    ]
