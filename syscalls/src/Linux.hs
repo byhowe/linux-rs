9304ad8690d91f744b35389f09e2d7ed56cc06e5
Ömer Faruk Çavuş <37745048+byhowe@users.noreply.github.com>
2026-08-31 20:55:19 +0300

    hs: Add data structure for architecture

--- syscalls/src/Linux.hs
+++ syscalls/src/Linux.hs
@@ -1,4 +1,17 @@
-module Linux (someFunc) where
+module Linux
+  ( Arch (..)
+  ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.Set (Set)
+import Data.Set qualified as Set
+
+data Arch = X86_64 | Aarch64 | Riscv64
+  deriving (Eq, Ord, Enum, Bounded)
+
+instance Show Arch where
+  show X86_64 = "x86_64"
+  show Aarch64 = "aarch64"
+  show Riscv64 = "riscv64"
+
+allArchs :: Set Arch
+allArchs = Set.fromList [minBound .. maxBound]
