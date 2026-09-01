3c9f2df06621dcac2eb8ce6578a92ba445e1ba09
Ömer Faruk Çavuş <37745048+byhowe@users.noreply.github.com>
2026-09-01 17:50:10 +0300

    hs: Add a verifier executable
    Idea is to verify that the values in the library are correct against the
    Linux source code. The gen-verify executable will eventually produce C
    code, which can be compiled.

---
+++ gen-verify/Main.hs
@@ -0,0 +1,29 @@
+module Main (main) where
+
+import Data.Char (toUpper)
+import Linux qualified
+import Linux.Definitions qualified
+
+flagMacroName :: String -> String -> String
+flagMacroName bitsetName flagName = case bitsetName of
+  _ -> map toUpper bitsetName ++ "_" ++ map toUpper flagName
+
+enumMacroName :: String -> String -> String
+enumMacroName enumName variantName = case enumName of
+  "access_mode" -> case variantName of
+    "read_only" -> "O_RDONLY"
+    "write_only" -> "O_WRONLY"
+    "read_write" -> "O_RDWR"
+    _ -> "O_" ++ map toUpper variantName
+  _ -> map toUpper enumName ++ "_" ++ map toUpper variantName
+
+bitsetMacroNames :: Linux.Bitset -> [String]
+bitsetMacroNames (Linux.Bitset bitsetName _ fields) = concatMap extractMacroNames fields
+ where
+  extractMacroNames :: Linux.BitsetField -> [String]
+  extractMacroNames (Linux.FieldFlag flagName _ _) = [flagMacroName bitsetName flagName]
+  extractMacroNames (Linux.FieldEnum _ _ (Linux.EnumDef enumName enumValues)) =
+    [enumMacroName enumName valName | Linux.EnumValue valName _ _ <- enumValues]
+
+main :: IO ()
+main = putStr . unlines $ bitsetMacroNames Linux.Definitions.mapBitset
