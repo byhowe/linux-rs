aaffd1a541d96b2977aef8b68e4a64f39985f348
Ömer Faruk Çavuş <37745048+byhowe@users.noreply.github.com>
2026-09-01 19:35:00 +0300

    gen-verify: Generate according to the architecture

--- gen-verify/Main.hs
+++ gen-verify/Main.hs
@@ -3,6 +3,7 @@
 module Main (main) where
 
 import Data.Char (toUpper)
+import Data.Set qualified as Set
 import Linux qualified
 import Linux.Definitions qualified
 import Text.Printf (printf)
@@ -17,21 +18,24 @@ enumMacroName "access_mode" "read_write" = "O_RDWR"
 enumMacroName "access_mode" variant = printf "O_%s" $ map toUpper variant
 enumMacroName enum variant = printf "%s_%s" (map toUpper enum) (map toUpper variant)
 
-bitsetMacroNames :: Linux.Bitset -> [(String, String)]
-bitsetMacroNames (Linux.Bitset bitsetName _ fields) =
+bitsetMacroNames :: Linux.Arch -> Linux.Bitset -> [(String, String)]
+bitsetMacroNames arch (Linux.Bitset bitsetName _ fields) =
   fields >>= \case
-    Linux.FieldFlag fName bit _ ->
-      [(flagMacroName bitsetName fName, printf "(1u << %d)" bit)]
-    Linux.FieldEnum bit _ (Linux.EnumDef eName vals) ->
-      [ (enumMacroName eName valName, printf "(%du << %d)" val bit)
-      | Linux.EnumValue valName val _ <- vals
+    Linux.FieldFlag fName bit archs ->
+      [ (flagMacroName bitsetName fName, printf "(1u << %d)" bit)
+      | arch `Set.member` archs
+      ]
+    Linux.FieldEnum bit _ (Linux.EnumDef enum vals) ->
+      [ (enumMacroName enum variant, printf "(%du << %d)" val bit)
+      | Linux.EnumValue variant val archs <- vals
+      , arch `Set.member` archs
       ]
 
 genVerifier :: Linux.Arch -> [Linux.Syscall] -> String
 genVerifier arch syscalls =
   unlines
     [ printf "_Static_assert(%s == %s, \"%s mismatch on %s\");" macroName value macroName (show arch)
-    | (macroName, value) <- bitsetMacroNames Linux.Definitions.mapBitset
+    | (macroName, value) <- bitsetMacroNames arch Linux.Definitions.mapBitset
     ]
 
 main :: IO ()
