713fd4298baa51ca8ab95baa644eec7ee5b78a74
Ömer Faruk Çavuş <37745048+byhowe@users.noreply.github.com>
2026-09-01 22:28:28 +0300

    gen-verify: Use forward piping to make the code readable

--- gen-verify/Main.hs
+++ gen-verify/Main.hs
@@ -5,7 +5,8 @@ module Main (main) where
 import Linux qualified
 
 import Data.Char (toUpper)
-import Data.List (nub)
+import Data.Containers.ListUtils (nubOrd)
+import Data.Function ((&))
 import Data.Set qualified as Set
 import Text.Printf (printf)
 
@@ -19,8 +20,8 @@ enumMacroName "access_mode" "read_write" = "O_RDWR"
 enumMacroName "access_mode" variant = printf "O_%s" $ map toUpper variant
 enumMacroName enum variant = printf "%s_%s" (map toUpper enum) (map toUpper variant)
 
-bitsetMacroNames :: Linux.Arch -> Linux.Bitset -> [(String, String)]
-bitsetMacroNames arch (Linux.Bitset bitsetName _ fields) =
+bitsetMacros :: Linux.Arch -> Linux.Bitset -> [(String, String)]
+bitsetMacros arch (Linux.Bitset bitsetName _ fields) =
   fields >>= \case
     Linux.FieldFlag fName bit archs ->
       [ (flagMacroName bitsetName fName, printf "(1u << %d)" bit)
@@ -46,12 +47,11 @@ genVerifier :: Linux.Arch -> [Linux.Syscall] -> String
 genVerifier arch syscalls =
   let
     types = concatMap (\(Linux.Syscall _ _ args ret _) -> ret : map Linux.type' args) syscalls
-    bitsets = nub . collectBitsets $ types
+    bitsets = types & collectBitsets & nubOrd & concatMap (bitsetMacros arch)
    in
     unlines
       [ printf "_Static_assert(%s == %s, \"%s mismatch on %s\");" macroName value macroName (show arch)
-      | bitset <- bitsets
-      , (macroName, value) <- bitsetMacroNames arch bitset
+      | (macroName, value) <- bitsets
       ]
 
 main :: IO ()
