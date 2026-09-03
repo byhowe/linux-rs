48b930bd287de46efdff63dd355f4fda331f936e
Ömer Faruk Çavuş <37745048+byhowe@users.noreply.github.com>
2026-09-03 17:59:41 +0300

    gen-syscalls: Add parser for tbl files

--- gen-syscalls/Main.hs
+++ gen-syscalls/Main.hs
@@ -1,6 +1,7 @@
 module Main (main) where
 
 import Control.Monad (unless)
+import Data.Foldable (traverse_)
 import System.Directory (doesDirectoryExist)
 import System.Environment (getArgs)
 import System.Exit (die)
@@ -25,14 +26,37 @@ validateOptions opts =
         doesDirectoryExist (linux opts)
             >>= flip unless (die $ "error: Linux tree path is not a directory: " ++ linux opts)
 
+data TblEntry = TblEntry
+    { number :: Int
+    , abi :: String
+    , name :: String
+    , entry :: String
+    , compat :: Maybe String
+    , noReturn :: Bool
+    }
+    deriving (Show)
+
+parseTblLine :: String -> TblEntry
+parseTblLine line = case words line of
+    [num, abi, name] -> TblEntry (read num) abi name "sys_ni_syscall" Nothing False
+    [num, abi, name, entry] -> TblEntry (read num) abi name entry Nothing False
+    [num, abi, name, entry, compat] -> TblEntry (read num) abi name entry (Just compat) False
+    [num, abi, name, entry, compat, "noreturn"] -> TblEntry (read num) abi name entry (Just compat) True
+    _ -> error $ "failed to parse syscall tbl line: " ++ line
+
+parseTbl :: String -> [TblEntry]
+parseTbl = map parseTblLine . filter isDataLine . lines
+  where
+    isDataLine [] = False
+    isDataLine ('#' : _) = False
+    isDataLine _ = True
+
 main :: IO ()
 main = do
-    opts <- validateOptions =<< either unknownFlag pure . parseOptions defaultOptions =<< getArgs
+    opts <- getArgs >>= either (\flag -> die $ "error: unknown flag: " ++ flag) validateOptions . parseOptions defaultOptions
 
     x86Table <- readFile $ linux opts </> "arch/x86/entry/syscalls/syscall_64.tbl"
     genericTable <- readFile $ linux opts </> "scripts/syscall.tbl"
 
-    putStrLn x86Table
-    putStrLn genericTable
-  where
-    unknownFlag flag = die $ "error: unknown flag: " ++ flag
+    traverse_ print $ parseTbl x86Table
+    traverse_ print $ parseTbl genericTable
