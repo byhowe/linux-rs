6c3e87e2062ad8156f7706ba900763c41d5f66f7
Ömer Faruk Çavuş <37745048+byhowe@users.noreply.github.com>
2026-09-08 01:38:14 +0300

    gen-syscalls: Extract syscall definitions from source code

--- gen-syscalls/Main.hs
+++ gen-syscalls/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE MultilineStrings #-}
 {-# LANGUAGE OverloadedRecordDot #-}
@@ -6,17 +7,29 @@ module Main (main) where
 
 import Linux qualified
 
-import Control.Monad (unless)
-import Data.Char (toUpper)
-import Data.Foldable (traverse_)
-import Data.List (intercalate)
+import Control.Monad (forM, guard, unless, (>=>))
+import Data.Char (digitToInt, isDigit, isSpace, toUpper)
+import Data.List (dropWhileEnd, intercalate, isPrefixOf)
 import Data.Map (Map)
 import Data.Map qualified as Map
 import Data.Maybe (mapMaybe)
-import System.Directory (doesDirectoryExist)
+import System.Directory (doesDirectoryExist, listDirectory)
 import System.Environment (getArgs)
 import System.Exit (die)
-import System.FilePath ((</>))
+import System.FilePath (takeExtension, (</>))
+
+import Text.ParserCombinators.ReadP
+    ( ReadP
+    , char
+    , eof
+    , many
+    , munch
+    , munch1
+    , readP_to_S
+    , satisfy
+    , string
+    , (+++)
+    )
 import Text.Printf (printf)
 
 data Options = Options
@@ -30,7 +43,7 @@ defaultOptions = Options{linux = "upstream/linux"}
 parseOptions :: Options -> [String] -> Options
 parseOptions opts [] = opts
 parseOptions opts ("--linux" : val : rest) = parseOptions (opts{linux = val}) rest
-parseOptions _ (flag : _) = error $ "unknown flag: " ++ flag
+parseOptions _ (flag : _) = error $ "error: unknown flag: " ++ flag
 
 validateOptions :: Options -> IO Options
 validateOptions opts =
@@ -54,8 +67,8 @@ parseTblLine line = case words line of
     [num, abi, name, entry] -> TblEntry (read num) abi name entry Nothing False
     [num, abi, name, entry, compat] -> TblEntry (read num) abi name entry (Just compat) False
     [num, abi, name, entry, compat, "noreturn"] -> TblEntry (read num) abi name entry (Just compat) True
-    [_num, _abi, _name, _entry, _compat, noreturn] -> error $ "invalid string in noreturn column: " ++ noreturn
-    _ -> error $ "failed to parse syscall tbl line: " ++ line
+    [_num, _abi, _name, _entry, _compat, noreturn] -> error $ "error: invalid string in noreturn column: " ++ noreturn
+    _ -> error $ "error: failed to parse syscall tbl line: " ++ line
 
 parseTbl :: String -> [TblEntry]
 parseTbl = map parseTblLine . filter isDataLine . lines
@@ -113,6 +126,90 @@ renderHeader =
         , "import Data.Map qualified as Map"
         ]
 
+listCFiles :: FilePath -> IO [FilePath]
+listCFiles dir = do
+    paths <- map (dir </>) <$> listDirectory dir
+    concat <$> forM paths \path -> do
+        isDir <- doesDirectoryExist path
+        if isDir
+            then listCFiles path
+            else pure [path | takeExtension path == ".c"]
+
+data SyscallDefine = SyscallDefine
+    { name :: String
+    , args :: [(String, String)]
+    }
+    deriving (Show)
+
+parseSC_ARG64 :: ReadP String
+parseSC_ARG64 = do
+    _ <- string "SC_ARG64"
+    _ <- char '('
+    name <- dropWhileEnd isSpace . dropWhile isSpace <$> munch (/= ')')
+    _ <- char ')'
+    eof
+    return (unwords . words $ name)
+
+expandArg :: String -> [String]
+expandArg arg = case readP_to_S parseSC_ARG64 arg of
+    (name, "") : _ -> ["u32", name ++ "_lo", "u32", name ++ "_hi"]
+    _ -> [arg]
+
+parseParens :: ReadP String
+parseParens = do
+    _ <- char '('
+    inner <- concat <$> many (parseParens +++ munch1 (\c -> c `notElem` ['(', ')']))
+    _ <- char ')'
+    return $ "(" ++ inner ++ ")"
+
+parseArg :: ReadP String
+parseArg = unwords . words . concat <$> many (parseParens +++ munch1 \c -> c `notElem` [',', '(', ')'])
+
+parseSyscallRaw :: ReadP String
+parseSyscallRaw = do
+    _ <- string "SYSCALL_DEFINE"
+    n <- digitToInt <$> satisfy isDigit
+    _ <- munch isSpace
+    args <- parseParens
+    return $ "SYSCALL_DEFINE" ++ show n ++ (unwords . words $ args)
+
+parseSyscall :: ReadP SyscallDefine
+parseSyscall = do
+    _ <- string "SYSCALL_DEFINE"
+    n <- digitToInt <$> satisfy isDigit
+
+    _ <- char '('
+
+    name <- parseArg
+    rawArgs <- many $ char ',' >> parseArg
+
+    _ <- char ')'
+
+    let expandedArgs = concatMap expandArg rawArgs
+    guard (length expandedArgs == 2 * n)
+    return $ SyscallDefine name (toPairs expandedArgs)
+  where
+    toPairs :: [String] -> [(String, String)]
+    toPairs (x : y : xs) = (x, y) : toPairs xs
+    toPairs [x] = [(x, "")]
+    toPairs [] = []
+
+extractSyscalls :: String -> [SyscallDefine]
+extractSyscalls content = go content True
+  where
+    go [] _ = []
+    go s@(c : cs) isStartOfLine
+        | isStartOfLine && ("SYSCALL_DEFINE" `isPrefixOf` s) =
+            case readP_to_S parseSyscall s of
+                (syscall, rest) : _ -> syscall : go rest False
+                [] ->
+                    let errorSyscall = case readP_to_S parseSyscallRaw s of
+                            (errorSyscallRaw, _) : _ -> errorSyscallRaw
+                            [] -> take 100 s
+                     in error $ printf "error: failed to parse SYSCALL_DEFINE macro: %s" errorSyscall
+        -- Otherwise consume a single character at a time until we see newline.
+        | otherwise = go cs (c == '\n')
+
 main :: IO ()
 main = do
     opts <- validateOptions . parseOptions defaultOptions =<< getArgs
@@ -136,8 +233,10 @@ main = do
             , sysName /= "_sysctl"
             ]
 
-    putStrLn renderHeader
-    traverse_ (putStrLn . renderSyscall) syscalls
+    -- putStrLn renderHeader
+    -- traverse_ (putStrLn . renderSyscall) syscalls
+    -- traverse_ putStrLn =<< listCFiles opts.linux
+    listCFiles opts.linux >>= mapM_ (readFile >=> mapM_ print . extractSyscalls)
   where
     x86TblPath = "arch/x86/entry/syscalls/syscall_64.tbl"
     genericTblPath = "scripts/syscall.tbl"
